54f922e7e0f70245a8b642ceebc9426b64b297ec
matheusdias1996 <55559654+matheusdias1996@users.noreply.github.com>
2023-01-11 20:42:23 -0800

    fix import

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 
 import lightning as pl
 
-from . import loadData, buildLabels, buildVocab
+from helper import loadData, buildLabels, buildVocab
 from data import reader
 from models import ProtCNN
 
