70d8c31d28edc864aeba0c311bc1deead7039546
matheusdias1996 <55559654+matheusdias1996@users.noreply.github.com>
2023-01-11 20:45:11 -0800

    fix imports in helper

--- helper.py
+++ helper.py
@@ -1,7 +1,7 @@
 import torch
 import pandas as pd
 
-from . import SequenceDataset
+from data import SequenceDataset
 
 def loadData(num_workers, word2id, fam2label, seq_max_len, data_dir, batch_size)->dict:
     """
