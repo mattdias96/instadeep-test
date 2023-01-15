c5240ebe6941bb9cae362d6b77f2c7421a71c81b
matheusdias1996 <55559654+matheusdias1996@users.noreply.github.com>
2023-01-15 10:54:30 -0800

    added batch_idx

--- models/protcnn.py
+++ models/protcnn.py
@@ -66,7 +66,7 @@ class ProtCNN(pl.LightningModule):
 
         return loss
     
-    def validation_step(self, batch:dict, **kwargs)->torch.Tensor:
+    def validation_step(self, batch:dict, batch_idx:int)->torch.Tensor:
         """
         Perform a forward pass on the validation input and compute the accuracy of the model's predictions.
         
