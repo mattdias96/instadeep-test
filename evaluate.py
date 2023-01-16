da9342445752be195f4bcafe3519c6a07ec55b7d
matheusdias1996 <55559654+matheusdias1996@users.noreply.github.com>
2023-01-15 19:43:26 -0800

    typo

--- evaluate.py
+++ evaluate.py
@@ -42,7 +42,7 @@ def main():
     model = ProtCNN(num_classes, args.lr, args.momentum, args.weight_decay, args.milestones, args.gamma) # make this flexible later
     model.load_state_dict(torch.load(args.model_weights_file_path)) # allow user to use own model later
     # Load the data
-    loader = loadData(args.num_workers, word2id, fam2label, args.seq_max_len, args.test_dir, args.batch_size)
+    loader = loadData(args.num_workers, word2id, fam2label, args.seq_max_len, args.train_dir, args.batch_size)
     # Evaluate predictions
     accuracy = evaluateModel(model, loader["test"])
     # Print result
