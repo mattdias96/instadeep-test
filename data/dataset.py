a4155709b9c1a37ba019d7f81fb1e409f4c4dca1
matheusdias1996 <55559654+matheusdias1996@users.noreply.github.com>
2023-01-16 14:37:27 -0800

    lint suggestions

--- data/dataset.py
+++ data/dataset.py
@@ -1,34 +1,39 @@
+"""
+This module contains the SequenceDataset class
+that transforms the raw data into a collection
+of sequences
+"""
 import numpy as np
 import torch
 
-from data import *
+from data import reader
 
 
 class SequenceDataset(torch.utils.data.Dataset):
     """
     A PyTorch dataset that represents a collection of sequences and their associated labels.
     sequences are one-hot encoded and padded or truncated to the `max_len`.
-    
+
     Parameters:
         - word2id (dict): a dictionary that maps aminoacid strings to unique integers.
         - fam2label (dict): a dictionary that maps family labels to unique integers.
         - max_len (int): the maximum length of the sequences in the dataset.
         - data_path (str): the path to a file containing the data to be read and processed.
         - split (str): dataset split i.e. 'train', 'dev' or 'test'
-        
+
     Attributes:
         - data (pd.DataFrame) : Dataframe containing the data after reading and processing
         - label (pd.DataFrame) : Dataframe containing the labels of the data
-        
+
     """
 
     def __init__(self, word2id:dict, fam2label:dict, max_len:int, data_path:str, split:str):
         self.word2id = word2id
         self.fam2label = fam2label
         self.max_len = max_len
-        
+
         self.data, self.label = reader(split, data_path)
-        
+
     def __len__(self)->int:
         """
         Returns the length of the dataset, which is the number of sequences.
@@ -38,10 +43,10 @@ class SequenceDataset(torch.utils.data.Dataset):
     def __getitem__(self, index:int)->dict:
         """
         Returns a dictionary containing the processed sequence and the target label.
-        
+
         Parameters:
             - index (int): Index of the sequence in the dataset
-            
+
         Returns:
             - dict: A dictionary containing the following keys,
                 - 'sequence' : preprocessed, padded, one-hot encoded sequence
@@ -49,35 +54,37 @@ class SequenceDataset(torch.utils.data.Dataset):
         """
         seq = self.preprocess(self.data.iloc[index])
         label = self.fam2label.get(self.label.iloc[index], self.fam2label['<unk>'])
-       
+
         return {'sequence': seq, 'target' : label}
-    
+
     def preprocess(self, text:str)->torch.Tensor:
         """
         Preprocess the sequence, by taking the slice of the text (sequence) of `max_len` length,
-        encodes it into IDs and pads it, and one-hot encodes the sequence and returns it in the permuted form.
-        
+        encodes it into IDs and pads it, and one-hot encodes the sequence and returns it in the
+        permuted form.
+
         Parameters:
             - text (str) : sequence as text
-            
+
         Returns:
             - torch.Tensor : preprocessed and one-hot encoded sequence
         """
         seq = []
-        
+
         # Encode into IDs
         for word in text[:self.max_len]:
             seq.append(self.word2id.get(word, self.word2id['<unk>']))
-                
+
         # Pad to maximal length
         if len(seq) < self.max_len:
             seq += [self.word2id['<pad>'] for _ in range(self.max_len - len(seq))]
-                
+
         # Convert list into tensor
         seq = torch.from_numpy(np.array(seq))
-            
-        # One-hot encode    
-        one_hot_seq = torch.nn.functional.one_hot(seq.to(torch.int64), num_classes=len(self.word2id), ) 
+
+        # One-hot encode
+        one_hot_seq = torch.nn.functional.one_hot(seq.to(torch.int64),
+                                                  num_classes=len(self.word2id), )
 
         # Permute channel (one-hot) dim first
         one_hot_seq = one_hot_seq.permute(1,0)
--- data/helper.py
+++ data/helper.py
@@ -1,46 +1,52 @@
+"""
+This module contains helper functions
+for the SequenceDataset class
+"""
 import os
 
-import pandas as pd
 from collections import Counter
+import pandas as pd
 
-data_dir = '/content/drive/MyDrive/InstaDeep/random_split' # this will be chosen by the user
 
 def reader(partition:str, data_path:str)->tuple:
     """
-    Reads files from a specified partition within a given data path, loads the contents of those files into a Pandas DataFrame,
-    concatenates all the dataframe into single one, and finally returns 2 specific columns of
-    the dataframe : 'sequence' (regressors) and 'family_accession' (target).
-    
+    Reads files from a specified partition within a given data path, loads the contents
+    of those files into a Pandas DataFrame, concatenates all the dataframe into single one,
+    and finally returns 2 specific columns of the dataframe : 'sequence' (regressors) and
+    'family_accession' (target).
+
     Parameters:
     partition (str) : The name of the partition to read files from
     data_path (str) : The path to the data
-    
+
     Returns:
-    Tuple : (pandas.Series, pandas.Series) - containing the 'sequence' and 'family_accession' columns of the concatenated DataFrame
+    Tuple : (pandas.Series, pandas.Series) - containing the 'sequence' and 'family_accession'
+    columns of the concatenated DataFrame
     """
     data = []
     for file_name in os.listdir(os.path.join(data_path, partition)):
-        with open(os.path.join(data_path, partition, file_name)) as file:
+        with open(os.path.join(data_path, partition, file_name), encoding="utf-8") as file:
             data.append(pd.read_csv(file, index_col=None, usecols=["sequence", "family_accession"]))
 
-    all_data = pd.concat(data)        
+    all_data = pd.concat(data)
 
     return all_data["sequence"], all_data["family_accession"]
 
 def get_amino_acid_frequencies(data: list)->pd.DataFrame:
     """
     Accepts a list of protein sequences and calculate the frequency of each amino acid in it
-    
+
     Parameters:
     data (List[str]) : A list of protein sequences
-    
+
     Returns:
-    pd.DataFrame : A dataframe with two columns 'AA' and 'Frequency' showing the amino acid and it's frequency respectively.
+    pd.DataFrame : A dataframe with two columns 'AA' and 'Frequency' showing the amino acid and
+    it's frequency respectively.
     """
     aa_counter = Counter()
-    
+
     for sequence in data:
         aa_counter.update(sequence)
-        
+
     return pd.DataFrame({'AA': list(aa_counter.keys()), 'Frequency': list(aa_counter.values())})
 
