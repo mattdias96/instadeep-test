907c6aa3d423b19e29bf9f5493daf73157dc07ec
matheusdias1996 <55559654+matheusdias1996@users.noreply.github.com>
2023-01-16 10:50:25 -0800

    fixing tests

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -15,7 +15,7 @@ class TestHelper(unittest.TestCase):
         """
         # Test that the buildVocab function correctly builds a vocabulary from AA strings to unique integers
         train_data = ["AAA", "AAC", "AGG"]
-        word2id = buildVocab(train_data, 2)
+        word2id = buildVocab(train_data, 0)
         self.assertIsInstance(word2id, dict)
         self.assertEqual(len(word2id), 3)
         self.assertEqual(word2id["A"], 2)
