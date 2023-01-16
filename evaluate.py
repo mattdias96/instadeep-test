bef2a40b30d21458827d3aaf6bf436b6f38d8bbc
matheusdias1996 <55559654+matheusdias1996@users.noreply.github.com>
2023-01-15 19:31:19 -0800

    changed directory to paths

--- evaluate.py
+++ evaluate.py
@@ -15,7 +15,8 @@ def main():
     # Add command line arguments
     parser.add_argument("--model", type=str, default = "default_protCNN") # make this flexible later
     parser.add_argument("--model_weights_file_path", type=str, required = True)
-    parser.add_argument('--data_dir', type=str, default='', help='path to the dataset to be evaluated')
+    parser.add_argument('--train_dir', type=str, default='C:\Users\mathe\Documents\random_split', help='path to the dataset to be trained')
+    parser.add_argument('--test_dir', type=str, default='', help='path to the dataset to be evaluated')
     parser.add_argument('--seq_max_len', type=int, default=120, help='maximum length of the aminoacid sequence')
     parser.add_argument('--rare_aa_count', type=int, default=5, help='number of Amino Acids to be considered rare')
     parser.add_argument('--batch_size', type=int, default=500, help='batch size')
@@ -29,7 +30,7 @@ def main():
     # Parse the command line arguments
     args = parser.parse_args()
     # Read train data files
-    valid_data, valid_targets = reader("train", args.data_dir) # change this in the reader function later
+    valid_data, valid_targets = reader("train", args.train_dir) # change this in the reader function later
     # Define dictionary from AA strings to unique integers
     word2id = buildVocab(valid_data, args.rare_aa_count)
     # Define dictionary mapping unique targets to consecutive integers
