bc2eaf88b9394250f4fdb4a47e923914ca041dbf
matheusdias1996 <55559654+matheusdias1996@users.noreply.github.com>
2023-01-15 20:25:47 -0800

    removed prints

--- evaluate.py
+++ evaluate.py
@@ -37,7 +37,6 @@ def main():
     fam2label = buildLabels(valid_targets)
     # Define number of classes in the dataset
     num_classes = len(fam2label)
-    print(num_classes)
     # Retrieve pretrained model
     model = ProtCNN(num_classes, args.lr, args.momentum, args.weight_decay, args.milestones, args.gamma) # make this flexible later
     model.load_state_dict(torch.load(args.model_weights_file_path)) # allow user to use own model later
