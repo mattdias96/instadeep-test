33be392daa68719daf00bcf3d9143e99b5fb753d
matheusdias1996 <55559654+matheusdias1996@users.noreply.github.com>
2023-01-16 10:47:09 -0800

    reviewing tests

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -18,9 +18,9 @@ class TestHelper(unittest.TestCase):
         word2id = buildVocab(train_data, 2)
         self.assertIsInstance(word2id, dict)
         self.assertEqual(len(word2id), 3)
-        self.assertEqual(word2id["AAA"], 0)
-        self.assertEqual(word2id["AAC"], 1)
-        self.assertEqual(word2id["AGG"], 2)
+        self.assertEqual(word2id["A"], 0)
+        self.assertEqual(word2id["C"], 1)
+        self.assertEqual(word2id["G"], 2)
 
     def testBuildLabels(self):
         """
